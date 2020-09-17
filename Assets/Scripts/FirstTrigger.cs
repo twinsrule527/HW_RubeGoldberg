394ad8cc8bf9f80fc76a3be9b37285b9affa3b8b
Eli <esc487@nyu.edu>
2020-09-17 11:48:05 -0500

    Test Scene Implementation
    These assets are part of a test scene within the file which I used to test out various physics elements. Are not included in the actual game.

---
+++ Assets/Scripts/FirstTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTrigger : MonoBehaviour
+{
+    public Transform cameraTransform;//Exposed in the Inspector
+    //automatically happen when something with a 2d rigid body enters this trigger
+    void OnTriggerEnter2D(Collider2D activator) {
+        //Camera changes to focus on the activator object
+        cameraTransform.position = activator.transform.position + new Vector3( 0f, 0f, -10f );
+    }
+    //When a 2Drigidbody exits the trigger
+    void OnTriggerExit2D(Collider2D activator) {
+            Debug.Log(activator.name + " left the scene");
+
+    }
+
+
+}
