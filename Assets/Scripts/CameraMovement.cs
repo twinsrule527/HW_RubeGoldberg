4a2c4d5febfd554a68fc0bc03346e3a77d8395e6
Eli <esc487@nyu.edu>
2020-09-21 11:25:18 -0500

    Phases 5 +, Many Visual Changes, Camera Size Change

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,8 @@ public static class Global {
 }
 public class CameraMovement : MonoBehaviour
 {
+
+    public Camera camera;
     //The Camera will follow whatever is the most recent object that is supposed to be followed
     
     //Several referential variables are used to make things easier
@@ -14,10 +16,16 @@ public class CameraMovement : MonoBehaviour
     float x;
     float y;
     float z;
+    float follow_size;
     Vector3 Position;
     Vector3 Movement = new Vector3(0f,0f,0f);
     
     Vector3 Movement_Normal;
+    
+    void Start() {
+        //Calls the Camera
+        camera = Camera.main;
+    }
     // Update is called once per frame
     void Update()
     {   //Sets positions (in case reference is needed)
@@ -32,25 +40,41 @@ public class CameraMovement : MonoBehaviour
             //It gets its position, and compares it to the camera's current position
             FollowPosition = Global.CameraFollow.transform.position;
             Movement = FollowPosition - Position;
-        }
-        if(Movement.magnitude < 0.5) {
-            //if the camera is close enough to the object, it just moves in
-            Movement_Normal = Movement;
             
+            //If this is one of the last triggers, it will increase the size of the camera
+            follow_size = Global.CameraFollow.transform.localScale.magnitude;
+            if(follow_size > 13 ) {
+                if(camera.orthographicSize < 15f ) {
+                    camera.orthographicSize = camera.orthographicSize + 0.02f;
+                }
+            }
+            else if(Global.CameraFollow.name == "TossBlock (PseudoBall)") {
+                Debug.Log("H");
+                if(camera.orthographicSize < 30f ) {
+                    camera.orthographicSize = camera.orthographicSize + 0.03f;
+                }
+            }
         }
-        else if(Movement.magnitude < 1) {
+        //if(Movement.magnitude < 0.5) {
+            //if the camera is close enough to the object, it just moves in
+           // Movement_Normal = Movement;
+            
+       // }
+        //else if(Movement.magnitude < 1) {
             //Otherwise it might normalize the vector
-            Movement_Normal = Vector3.Normalize(Movement) / 2;
-        }
-        else {
+           // Movement_Normal = Vector3.Normalize(Movement) / 2;
+      //  }
+       // else {
             //Or, if the object is far enough away, it will bring itself halfway there
-            Movement_Normal = Movement / 4;
+           // Movement_Normal = Movement / 4;
 
-        }
+       // }
+       Movement_Normal = Movement / 16;
         //Then, finally, the camera moves
         this.transform.position += Movement_Normal;
         //Finally, we reset the z value, so that the camera will not be in the way of any objects
         this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,-50.0f);
-
+    
+        
     }
 }
---
+++ Assets/Scripts/RopeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour
+{
+    //This script is used to create a rope which the object swings from
+    //Needed vectors
+    Vector2 end1 = new Vector2(0f,0f);
+    Vector2 end2 = new Vector2(1f,10f);
+    DistanceJoint2D dj2D;
+
+    Color black = new Color(0f, 0f, 0f, 1f);
+     // Start is called before the first frame update
+
+    void Start()
+    {
+        //Creating the Line Renderer and all of its base attributes
+        LineRenderer line = gameObject.AddComponent<LineRenderer>();
+        
+        line.material = new Material(Shader.Find("Sprites/Default"));
+
+        line.startWidth = 0.05f;
+
+        line.startColor = black;
+
+        line.endColor = black;
+
+        dj2D = GetComponent<DistanceJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+       
+        //end1 = this.dj2D.anchor;
+        end1 = this.transform.position;
+        end2 = this.dj2D.connectedAnchor;
+
+
+        line.SetPosition(0, end1);
+        line.SetPosition(1, end2);
+    }
+}
